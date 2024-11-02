3d3528f927341c09dd988363a2ba4de04e4e3ff9
Danish <muhammeddanishkv05@gmail.com>
2024-11-03 01:01:34 +0530

    feat: added axios interceptors, services,

---
+++ src/services/authService.js
@@ -0,0 +1,58 @@
+import { showToast } from "../utils/showToast";
+import api from "./api";
+
+export const register = async (username, email, password) => {
+  try {
+    const res = api.post("api/register/", {
+      username,
+      email,
+      password,
+    });
+    showToast(200, "Registered succussfully...");
+    return res.data;
+  } catch (error) {
+    handleError(error);
+    throw error;
+  }
+};
+
+export const login = async (username, password) => {
+  try {
+    const res = api.post("api/token/", {
+      username,
+      password,
+    });
+    showToast(200, "Welcome Back!");
+    return res.data;
+  } catch (error) {
+    handleError(error);
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  localStorage.clear();
+  showToast(200, "Thank You..");
+};
+
+const handleError = (error) => {
+  if (error.response) {
+    const status = error.response.status;
+    const detail = error.response.data?.detail;
+
+    if (status === 401) {
+      showToast(400, "Unauthorized access.");
+    } else {
+      showToast(status, detail || "An unexpected error occurred.");
+    }
+  } else {
+    showToast(400, "Network error. Please try again.");
+    console.error("Error:", error);
+  }
+};
+
+export default {
+  register,
+  login,
+  logout,
+};
